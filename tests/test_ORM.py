1feb390a3a6ef0423fb48b319cb181cccbd2014d
Kevin Murray <spam@kdmurray.id.au>
2014-02-10 16:00:24 +1100

    clean up tests a bit

--- tests/test_ORM.py
+++ tests/test_ORM.py
@@ -1,24 +1,35 @@
 import unittest
 import traitcapture
-from traitcapture import TableBase, Accession, Species, User
+from traitcapture import orm
+from traitcapture.orm import TableBase, Accession, Species, User
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import create_engine
 from datetime import datetime
 from tempfile import mkstemp
+from shutil import copy2, rmtree, copytree
 import os
+from os import path, makedirs
 
-TEMPLATE_DB = "./Tests/test.db"
+
+TEST_DIR = path.dirname(__file__)
+DATA_DIR = path.join(TEST_DIR, "data")
+OUT_DIR = path.join(TEST_DIR, "out")
+TEMPLATE_DB = path.join(DATA_DIR, "test.db")
 
 
-class TestORM(unittest.TestCase):
-    
-    def test_ormsetup(self):
-        engine = create_engine("sqlite://")
-        session = sessionmaker(bind=engine)()
-        TableBase.metadata.create_all(engine)
-        self.assertTrue(traitcapture.main())
-
+class BaseTest(unittest.TestCase):
+    """Base of all tests, includes tmp dir creation/deletion"""
     def setUp(self):
+        makedirs(OUT_DIR)
+
+    def tearDown(self):
+        rmtree(OUT_DIR)
+
+
+class BaseORMTest(BaseTest):
+    """Base of all ORM-testing cases, creates a db engine"""
+    def setUp(self):
+        super(BaseTest, self).setUp()
         db_fh, self.db_fn = mkstemp()
         db_fh = os.fdopen(db_fh, "wb")
         template_fh = open(TEMPLATE_DB, "rb")
@@ -26,14 +37,30 @@ class TestORM(unittest.TestCase):
         template_fh.close()
         db_fh.close()
         self.engine = create_engine("sqlite:///%s" % self.db_fn)
-        traitcapture.Session = sessionmaker(bind=self.engine)
-        self.session = traitcapture.Session()
+        orm.Session = sessionmaker(bind=self.engine)
+        self.session = orm.Session()
 
     def tearDown(self):
+        super(BaseTest, self).tearDown()
         self.session.close()
         os.remove(self.db_fn)
 
-    def test_species(self):
+
+class TestORMSetup(BaseTest):
+    # don't explicity use unittests here, we just want errors to be raised
+    def test_ormsetup_inmemory(self):
+        engine = create_engine("sqlite://")
+        session = sessionmaker(bind=engine)()
+        TableBase.metadata.create_all(engine)
+
+    def test_orm_generation_function(self):
+        db_fn = path.join(OUT_DIR, "test.db")
+        orm.main(db_fn)
+        self.assertTrue(path.exists(db_fn))
+
+class TestSpecies(BaseORMTest):
+
+    def test_add_species(self):
         record = {
                 "genus": "Eucalyptus",
                 "species": "scoparia",
@@ -50,8 +77,9 @@ class TestORM(unittest.TestCase):
         self.session.add(record_instance)
         self.session.commit()
         self.assertEqual(record_instance.id, 2)
-        
-    def test_user(self):
+
+class TestUser(BaseORMTest):
+    def test_add_user(self):
         record = {
                 "user_name": "testuser1",
                 "given_name": "test1",
@@ -71,7 +99,8 @@ class TestORM(unittest.TestCase):
         self.session.commit()
         self.assertEqual(record_instance.id, 2)
 
-    def test_accession(self):
+class TestAccession(BaseORMTest):
+    def test_add_accession(self):
         record = {
                 "accession_name": "test_accession",
                 "species_id": 1,
@@ -89,7 +118,3 @@ class TestORM(unittest.TestCase):
         self.session.commit()
         self.assertEqual(record_instance.id, 1)
 
-
-if __name__ == "__main__":
-    runner = unittest.TextTestRunner(verbosity=5)
-    unittest.main(testRunner=runner)
